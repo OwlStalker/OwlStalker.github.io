ea0e6dfbd296a94bc7d6cd694be1fc5d3d5c9842
OwlStalker <dkravchu@uncc.edu>
2022-04-08 13:04:40 -0400

    ac

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -1,13 +1,55 @@
 $(document).ready(function() {
 
 
-    // preload the image for each link
-
+    /*// preload the image for each link
+    $('#image_list a').each(function(){
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr('href');
+    });
+    
     // set up the event handlers for each link
+    $('#image_list a').click(function(evt) {
 
-            // get the image URL and caption for each image and animate the caption
+        //swap image
+        var imageURL = $(this).attr("href");
+        $('#main-image').attr('src', imageURL);
 
-        // cancel the default action of each link
+        //swap caption
+        var caption = $(this).attr('title');
+        $('#caption').text(caption);
+    });
+    */
+    
+    var image, imageCounter = 0, imageCache = [];
+    $('#image_list a').each(function() {
+        image = new Image();
+        image.src = $(this).attr('href');
+        image.title = $(this).attr('title');
+        imageCache[imageCounter] = image;
+        imageCounter++;
+    });
+
+    //Start slideshow
+    imageCounter = 0;
+    var nextImage;
+
+    setInterval(function(){
+
+        $("#caption").fadeOut(1000);
+        $("#main-image").fadeOut(1000, function(){
+            imageCounter = (imageCounter + 1) % imageCache.length;
+            nextImage = imageCache[imageCounter];
+
+            $("#main-image").attr('src', nextImage.src).fadeIn(1000);
+            $("#caption").text(nextImage.title).fadeIn(1000);
+        });
+    }, 3000);
+
+    // get the image URL and caption for each image and animate the caption
+
+
+
+    // cancel the default action of each link
 
 
 // move the focus to the first link
